47bacbba28a962adf98bb4571aff1e0a690c7375
papi <ryanmuuo91@gmail.com>
2019-01-10 15:29:42 +0300

    add all changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailsComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
